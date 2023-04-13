efdfb401bd3cfdf98e5f4a85a372559ff815fc22
Callie Barnes <callie.barnes@g.austincc.edu>
2023-04-12 21:13:06 -0500

    added Tetrominoe class and draw_board and draw_tetromino

--- tetris.py
+++ tetris.py
@@ -52,7 +52,13 @@ Z = [
     [1, 1, 0],
     [0, 1, 1]
 ]
-TETROMINOES = [I, J, L, O, S, T, Z];
+TETROMINOES = [I, J, L, O, S, T, Z]
+
+# Board
+BOARD_WIDTH = 10
+BOARD_HEIGHT = 20
+def create_board():
+    return [[0 for _ in range(BOARD_WIDTH)] for _ in range(BOARD_HEIGHT)]
 
 
 class Tetris:
@@ -80,8 +86,29 @@ class Tetris:
 
     def draw(self):
         self.screen.fill(BLACK)
-        # Draw the game board, tetrominoes, and other elements here
+        self.board = create_board()
+        self.draw_board()
+        self.draw_tetromino(random.choice(TETROMINOES), 0, 0)
         pygame.display.flip()
+    
+    def draw_board(self):
+        for y in range(BOARD_HEIGHT):
+            for x in range(BOARD_WIDTH):
+                if self.board[y][x] == 0:
+                    pygame.draw.rect(self.screen, WHITE, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE), 1)
+                else:
+                    pygame.draw.rect(self.screen, WHITE, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+    
+    def draw_tetromino(self, tetromino, x, y):
+        for i in range(len(tetromino)):
+            for j in range(len(tetromino[i])):
+                if tetromino[i][j] == 1:
+                    pygame.draw.rect(self.screen, WHITE, (x + j * GRID_SIZE, y + i * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+    
+    def draw_text(self, text, x, y, color):
+        font = pygame.font.SysFont('Arial', 30)
+        text = font.render(text, True, color)
+        self.screen.blit(text, (x, y))
 
 if __name__ == '__main__':
     tetris = Tetris()
